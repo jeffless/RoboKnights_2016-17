82a8b7710a4be4dd013db2f8bbd2bbaca021f817
Jeffrey Lin <greenmarimba@gmail.com>
2016-12-17 11:08:51 -0800

    Updates 12-17-16
    Added program to test the flywheel's maximum load

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelTest.java
@@ -0,0 +1,123 @@
+/* Copyright (c) 2015 Qualcomm Technologies Inc
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package org.firstinspires.ftc.teamcode;
+
+/*
+
+TODO:
+
+Add option to (try to) dump blocks in low goal before climbing ramp on our side
+Add option to try to drive to other side of field (make sure its allowed), climb up the ramp of our color there, and position for scoring in the medium goal. probably terminate if gyro reads too much directional change.
+Add ultrasonic sensor when we add rescue beacon detection?
+*/
+
+//Hello world.
+
+//NOTE: Do NOT put waitFullCycle in loops. Only put in between other stuff
+
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "FlywheelTest", group = "Tools")
+//@Disabled
+public class FlywheelTest extends LinearOpMode
+{
+    protected DcMotor flywheelLeft;
+    protected DcMotor flywheelRight;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public void runOpMode ()
+    {
+        /*flywheelLeft = hardwareMap.dcMotor.get("fl");
+        flywheelLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        flywheelLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //flywheelLeft.setMaxSpeed();
+        flywheelRight = hardwareMap.dcMotor.get("fr");
+        flywheelRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        flywheelRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //flywheelRight.setMaxSpeed();
+
+        try
+        {
+            waitForStart();
+        }
+        catch (Exception e)
+        {
+
+        }
+        while (opModeIsActive())
+        {
+            flywheelLeft.setPower(.8);
+            flywheelRight.setPower(.8);
+        } */
+
+        flywheelLeft = hardwareMap.dcMotor.get("fl");
+        flywheelRight = hardwareMap.dcMotor.get("fr");
+
+        flywheelLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        flywheelRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        try
+        {
+            waitForStart();
+        }
+
+        catch (Exception e)
+        {
+
+        }
+
+        flywheelLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        flywheelRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        runtime.reset();
+
+        flywheelLeft.setPower(1.0);
+        flywheelRight.setPower(1.0);
+        while(opModeIsActive() && (runtime.time() < 10))
+        {
+            telemetry.addData("Left Encoder", "%4.1f:  %d counts", runtime.time(), flywheelLeft.getCurrentPosition());
+            telemetry.addData("Right Encoder", "%4.1f:  %d counts", runtime.time(), flywheelRight.getCurrentPosition());
+            telemetry.update();
+        }
+
+        flywheelLeft.setPower(0.0);
+        flywheelRight.setPower(0.0);
+
+        telemetry.addData("Left Encoder", "%5.0f Counts Per Second", (double)(flywheelLeft.getCurrentPosition()) / runtime.time());
+        telemetry.addData("Right Encoder", "%5.0f Counts Per Second", (double)(flywheelRight.getCurrentPosition()) / runtime.time());
+        telemetry.update();
+        while (opModeIsActive());
+    }
+}

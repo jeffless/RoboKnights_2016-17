73105d79d80b17a69d5198a479aa33c5fb1c4f9e
Jeffrey Lin <greenmarimba@gmail.com>
2017-01-28 22:28:17 -0800

    Updates 1-28-2017
    Cleaned up FlywheelTest and made shooter control into self-contained,
    reusable classes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelTest.java
@@ -37,7 +37,6 @@ Add ultrasonic sensor when we add rescue beacon detection?
 //NOTE: Do NOT put waitFullCycle in loops. Only put in between other stuff
 
 
-import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.*;
 import com.qualcomm.robotcore.hardware.*;
@@ -47,380 +46,135 @@ import com.qualcomm.robotcore.util.Range;
 //@Disabled
 public class FlywheelTest extends LinearOpMode
 {
-    protected DcMotor flywheelLeft;
-    protected DcMotor flywheelRight;
-    protected DcMotor sweeperMotor;
+    private DcMotor flywheelLeft;
+    private DcMotor flywheelRight;
 
-    protected Servo doorServo;
+    private static final double TOLERANCE = 0.5e-7;
+    private static final double TARGET_VELOCITY = 1.1e-6;
 
-    /*double tbhI = 300.0;
-    private double kP = 0.4;
-    private double kI = 0.0;
-    //private double kD = 380000;
-    private double kD = 0.0;
-
-    private double integral = 0.0;
-    private double derivative = 0.0;
-
-    private double motorOut = 0.0;
-    private double fTarget = 1.1e-6;
-    private double fVelocity = 0.0;
-    private double fError = 0.0;
-    private double fLastError = 0.0;
-    private double tbh = 0.0;
-
-    private int fEncoder = 0;
-    private int fLastEncoder = 0;
-
-    private long fVelocityTime = 0;
-    private long fLastVelocityTime = 0;
-
-    private double place = 0.1;
-
-    private boolean firstCross;
-
-    private double tolerance = 0.5e-7;
-
-    private double targetVoltage = 12.5;
-    private double voltage;
-    */
-
-    protected static final double TARGET_VOLTAGE = 12.5;
-    protected static final double TARGET_SPEED = 1.1e-6;
-
-    protected static final double vP = 0.18;
-    protected static final double sP = 0.37;
-    protected static final double sI = 0.0;
-    protected static final double sD = 0.0;
-
-    protected static double voltage = 0;
-    protected static double speed = 0;
-    protected static double integral = 0;
-    protected static double lastError;
-    protected static double lastTime;
-    protected static double lastEncoder;
-
-    private static final String TAG = "MyActivity";
-
-    public void runOpMode ()
+    @Override
+    public void runOpMode() throws InterruptedException
     {
         flywheelLeft = hardwareMap.dcMotor.get("flywheel1");
         flywheelLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        //flywheelLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //flywheelLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         flywheelRight = hardwareMap.dcMotor.get("flywheel2");
         flywheelRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        //flywheelRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //flywheelRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        sweeperMotor = hardwareMap.dcMotor.get("sweeper1");
-        sweeperMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        PIDCalculator velocityPID = new PIDCalculator();
+        VelocityCalculator flywheelVelocity = new VelocityCalculator();
+        BangBangCalculator velocityBangBang = new BangBangCalculator();
 
-        doorServo = hardwareMap.servo.get("dServo");
+        waitForStart();
 
-        //firstCross = true;
-
-        try
+        /*
+        PID Example
+         */
+        while(opModeIsActive())
         {
-            waitForStart();
+            flywheelVelocity.setParameters(System.nanoTime(), flywheelRight.getCurrentPosition());
+            double currentVelocity = flywheelVelocity.getVelocity();
+            double currentError = TARGET_VELOCITY - currentVelocity;
+
+            velocityPID.setParameters(0.37, 0.1, 0.0, currentError, 0.85);
+            double motorOut = velocityPID.getPID();
+
+            motorOut = Range.clip(motorOut, 0, 1);
+            flywheelLeft.setPower(motorOut);
+            flywheelRight.setPower(motorOut);
         }
-        catch (Exception e)
-        {
 
-        }
-
-        ShootThread shoot = new ShootThread();
-        while (opModeIsActive())
+        /*
+        Bang Bang Example
+         */
+        while(opModeIsActive())
         {
-            voltage = batteryVoltage();
-            sleep(100);
-            sweeperMotor.setPower(1);
-            sleep(3000);
-            sweeperMotor.setPower(0);
-            shoot.mResume();
-            sleep(1200);
-            doorServo.setPosition(1.0);
-            sleep(1200);
-            shoot.mSuspend();
-            sleep(100);
+            flywheelVelocity.setParameters(System.nanoTime(), flywheelRight.getCurrentPosition());
+            double currentVelocity = flywheelVelocity.getVelocity();
+
+            velocityBangBang.setParameters(currentVelocity, TARGET_VELOCITY, 0.84, 0.9, TOLERANCE);
+            double motorOut = velocityBangBang.getBangBang();
+
+            motorOut = Range.clip(motorOut, 0, 1);
+            flywheelLeft.setPower(motorOut);
+            flywheelRight.setPower(motorOut);
         }
     }
 
-    /*
-    private void printVelocity()
+    public class PIDCalculator
     {
-        fVelocityTime = System.nanoTime();
-        fEncoder = flywheelRight.getCurrentPosition();
-        fVelocity = (double)(fEncoder - fLastEncoder) / (fVelocityTime - fLastVelocityTime);
+        private double kP, kI, kD, error, constant;
+        private double lastError;
+        private double integral, derivative;
 
-        telemetry.addData("3", "Time " + fVelocityTime);
-        telemetry.addData("4", "Encoder " + fEncoder);
-        telemetry.addData("5", "Last Encoder " + fLastEncoder);
-        telemetry.addData("6", "Encoder Change " + (fEncoder - fLastEncoder));
-        telemetry.addData("7", "Time Change " + (fVelocityTime - fLastVelocityTime));
-        telemetry.addData("8", "Velocity " + fVelocity);
-        telemetry.addData("9", "Voltage " + batteryVoltage());
-        telemetry.update();
-
-        fLastEncoder = fEncoder;
-        fLastVelocityTime = fVelocityTime;
-    }
-
-    private void setFPower(double power)
-    {
-        flywheelLeft.setPower(power);
-        flywheelRight.setPower(power);
-    }
-
-    //TBH CODE
-    private void calculateTBH()
-    {
-        fVelocityTime = System.nanoTime();
-        fEncoder = flywheelLeft.getCurrentPosition();
-        fVelocity = ((double)(fEncoder - fLastEncoder) / (fVelocityTime - fLastVelocityTime));
-        fError = fTarget - fVelocity;
-
-        motorOut = motorOut  + (fError * tbhI);
-
-        motorOut = Range.clip(motorOut, 0 ,1);
-
-        if(Math.signum(fError) != Math.signum(fLastError))
+        public void setParameters(double kP, double kI, double kD, double error, double constant)
         {
-            if(firstCross)
+            this.kP = kP;
+            this.kI = kI;
+            this.kD = kD;
+            this.error = error;
+            this.constant = constant;
+        }
+
+        public double getPID()
+        {
+            derivative = error - lastError;
+            integral += error;
+            lastError = error;
+            return (kP * error) + (kI * integral) + (kD * derivative) + constant;
+        }
+    }
+
+    public class BangBangCalculator
+    {
+        private double target;
+        private double velocity;
+        private double lowerPower, higherPower;
+        private double tolerance;
+
+        public void setParameters(double target, double velocity, double lowerPower, double higherPower, double tolerance)
+        {
+            this.target = target;
+            this.velocity = velocity;
+            this.lowerPower = lowerPower;
+            this.higherPower = higherPower;
+            this.tolerance = tolerance;
+        }
+
+        public double getBangBang()
+        {
+            if(velocity >= (target + tolerance))
             {
-                motorOut = 0.3;
-                firstCross = false;
+                return lowerPower;
             }
 
             else
             {
-                motorOut = 0.5 * (motorOut + tbh);
+                return higherPower;
             }
-
-            tbh = motorOut;
-        }
-
-        fLastError = fError;
-
-        setFPower(motorOut );
-    }
-
-    private void calculatePID()
-    {
-        fVelocityTime = System.nanoTime();
-        fEncoder = flywheelLeft.getCurrentPosition();
-        fVelocity = (double)(fEncoder - fLastEncoder) / (fVelocityTime - fLastVelocityTime);
-        fError = fTarget - fVelocity;
-
-        integral += fError;
-        if(fError == 0)
-        {
-            integral = 0;
-        }
-
-        derivative = fError - fLastError;
-
-        fLastError = fError;
-        fLastEncoder = fEncoder;
-        fLastVelocityTime = fVelocityTime;
-
-        motorOut = (kP * fError) + (kI * integral) + (kD * derivative) + .8;
-
-        motorOut = Range.clip(motorOut, 0.0, 1.0);
-
-        telemetry.addData("1", "kP " + (kP * fError));
-        telemetry.addData("2", "Error " + fError);
-        telemetry.addData("3", "Time " + fVelocityTime);
-        telemetry.addData("4", "Encoder " + fEncoder);
-        telemetry.addData("5", "Last Encoder " + fLastEncoder);
-        telemetry.addData("6", "Encoder Change " + (fEncoder - fLastEncoder));
-        telemetry.addData("7", "Time Change " + (fVelocityTime - fLastVelocityTime));
-        telemetry.addData("8", "Velocity " + fVelocity);
-        telemetry.addData("9", "Result " + motorOut);
-        telemetry.update();
-
-        Log.wtf(TAG, String.valueOf(fError));
-
-        setFPower(motorOut);
-    }
-
-
-    public void bangBang()
-    {
-        fVelocityTime = System.nanoTime();
-        fEncoder = flywheelLeft.getCurrentPosition();
-        fVelocity = (double)(fEncoder - fLastEncoder) / (fVelocityTime - fLastVelocityTime);
-
-        if(fVelocity >= (fTarget + tolerance))
-        {
-            setFPower(.84);
-        }
-
-        else if(fVelocity < (fTarget - tolerance))
-        {
-            setFPower(.9);
-        }
-
-        fLastEncoder = fEncoder;
-        fLastVelocityTime = fVelocityTime;
-    }
-
-    public void adjustPID()
-    {
-        if(gamepad1.right_bumper)
-        {
-            kP += (1.0 * place);
-        }
-
-        if(gamepad1.left_bumper)
-        {
-            kP -= (1.0 * place);
-        }
-
-        if(gamepad1.right_trigger > 0.7)
-        {
-            place *= 10.0;
-        }
-
-        if(gamepad1.left_trigger > 0.7)
-        {
-            place *= 0.1;
         }
     }
 
-    public void printVoltage()
+    public class VelocityCalculator
     {
-        double voltage = this.hardwareMap.voltageSensor.iterator().next().getVoltage();
-        telemetry.addData("1", "Voltage " + voltage);
-        telemetry.update();
-        sleep(200);
-    }
+        private long time, encoder;
 
+        private long lastEncoder, lastTime;
 
-    public double batteryVoltage()
-    {
-        return this.hardwareMap.voltageSensor.iterator().next().getVoltage();
-    }
-
-    public void voltageProportional()
-    {
-        double kP = .15;
-        double error = targetVoltage - voltage;
-        motorOut = (error * kP) + .88;
-        motorOut = Range.clip(motorOut, 0, 1);
-        setFPower(motorOut);
-    } */
-
-    private void setFPower(double power)
-    {
-        flywheelLeft.setPower(power);
-        flywheelRight.setPower(power);
-    }
-
-    public final void shoot()
-    {
-        double vError = TARGET_VOLTAGE - voltage;
-        double vMotorOut = (vError * vP) + .82;
-        vMotorOut = Range.clip(vMotorOut, 0, 1);
-
-        double sError = TARGET_SPEED - flywheelSpeed();
-        double derivative = sError - lastError;
-        integral += sError;
-        lastError = sError;
-
-        double sMotorOut = (sP * sError) + (sI * integral) + (sD * derivative) + vMotorOut;
-        sMotorOut = Range.clip(sMotorOut, 0.0, 1.0);
-        setFPower(sMotorOut);
-        sleep(50);
-    }
-
-    public final void stopShooting()
-    {
-        setFPower(0);
-    }
-
-    public double flywheelSpeed()
-    {
-        long time = System.nanoTime();
-        long encoder = flywheelRight.getCurrentPosition();
-        double speed = (encoder - lastEncoder) / (time - lastTime);
-
-        lastEncoder = encoder;
-        lastTime = time;
-
-        return speed;
-    }
-
-    public double batteryVoltage()
-    {
-        return this.hardwareMap.voltageSensor.iterator().next().getVoltage();
-    }
-
-    public class ShootThread implements Runnable
-    {
-        Thread thrd;
-
-        private boolean suspended;
-        private boolean stopped;
-
-        private boolean opModeHasBeenActive = false;
-
-        ShootThread()
+        public void setParameters(long time, long encoder)
         {
-            thrd = new Thread(this);
-            suspended = true;
-            stopped = false;
-            voltage = batteryVoltage();
-            thrd.start();
+            this.time = time;
+            this.encoder = encoder;
         }
 
-        public void run()
+        public double getVelocity()
         {
-            try
-            {
-                while(!opModeHasBeenActive || opModeIsActive()) //should keep running
-                {
-                    if (opModeIsActive()) opModeHasBeenActive = true;
-                    shoot();
+            double velocity = (double) (encoder - lastEncoder) / (time - lastTime);
 
-                    synchronized (this)
-                    {
-                        while(suspended)
-                        {
-                            if (opModeIsActive()) opModeHasBeenActive = true;
-                            stopShooting();
-                            wait();
-                        }
-                        if(stopped) break;
-                    }
-                }
-            } catch(InterruptedException exc){}
-        }
+            lastEncoder = encoder;
+            lastTime = time;
 
-        synchronized void mStop()
-        {
-            stopped = true;
-            suspended = false;
-            notify();
-        }
-
-        synchronized void mSuspend()
-        {
-            suspended = true;
-        }
-
-        synchronized void mResume ()
-        {
-            suspended = false;
-            voltage = batteryVoltage();
-            integral = 0;
-            notify();
-        }
-
-        public boolean isSuspended()
-        {
-            return suspended;
+            return velocity;
         }
     }
 }
